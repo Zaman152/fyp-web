8ef744636d02690030a0776ae1fc1d29532f593f
AliZamanKhan <134669375+Zaman152@users.noreply.github.com>
2023-12-16 12:03:11 +0500

    updated
    sad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: UserAuthComponent,
+    path: 'user-auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAuthComponent
   ],
   imports: [
     BrowserModule,
